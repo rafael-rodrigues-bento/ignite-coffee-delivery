ea898ba3ec147388cd4787b0f5f12eab29a22ba1
rafael <rafael.rodrigues.bento@outlook.com>
2023-01-09 22:02:27 -0300

    Zod

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -5,18 +5,38 @@ import { AddressInfo } from "./components/AddressInfo";
 import { Payment } from "./components/Payment";
 import { CoffeeSelected } from "./components/CoffeeSelected";
 
-export function Checkout() {
-  const methods = useForm();
-  const { handleSubmit } = useForm();
+import { zodResolver } from "@hookform/resolvers/zod"
+import * as zod from "zod"
 
-  function handleCreateNewOrder(data: any) {
+const newOrderFormValidationSchema = zod.object({
+  cep: zod.string().min(8, "Informe um Cep"),
+  street: zod.string().min(1, "Informe a sua Rua"),
+  number: zod.string().min(1, "Informe o seu Número"),
+  complement: zod.string(),
+  district: zod.string().min(1, "Informe o seu Bairro"),
+  city: zod.string().min(1, "Informe a sua Cidade"),
+  uf: zod.string().min(1, "Informe a UF"),
+  methodPayment: zod.string().min(1, "Informe o método de pagamento")
+})
+
+type NewOrderFormData = zod.infer<typeof newOrderFormValidationSchema>
+
+export function Checkout() {
+  const newOrder = useForm<NewOrderFormData>({
+    resolver: zodResolver(newOrderFormValidationSchema),
+  });
+
+  const { handleSubmit, reset } = newOrder
+
+  function handleCreateNewOrder(data: NewOrderFormData) {
     console.log(data)
+    reset()
   }
 
   return (
     <CheckoutContainer>
-      <FormProvider {...methods} >
-        <form onSubmit={methods.handleSubmit(handleCreateNewOrder)}>
+      <FormProvider {...newOrder} >
+        <form onSubmit={handleSubmit(handleCreateNewOrder)}>
           <div>
             <h1>Complete o seu pedido</h1>
             <AddressInfo />
